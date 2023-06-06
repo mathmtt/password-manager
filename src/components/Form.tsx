e45611e661e090e0bcbeb744c81c7ee9bdb5d8fd
Matheus Motta <matheusmotta136@gmail.com>
2023-06-06 15:31:44 -0300

    Requisito 03

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+  const [register, setRegister] = useState(false);
   return (
-    <form>
-      <label htmlFor="inputText">
-        Nome do serviço
-        <input type="text" id="inputText" />
-      </label>
-      <label htmlFor="inputLogin">
-        Login
-        <input type="text" id="inputLogin" />
-      </label>
-      <label htmlFor="inputPassword">
-        Senha
-        <input type="password" id="inputPassword" />
-      </label>
-      <label htmlFor="inputURL">
-        URL
-        <input type="text" id="inputURL" />
-      </label>
-      <button type="submit">Cadastrar</button>
-      <button type="reset">Cancelar</button>
-    </form>
+    <div>
+      {
+        register ? (
+          <form>
+            <label htmlFor="inputText">
+              Nome do serviço
+              <input type="text" id="inputText" />
+            </label>
+            <label htmlFor="inputLogin">
+              Login
+              <input type="text" id="inputLogin" />
+            </label>
+            <label htmlFor="inputPassword">
+              Senha
+              <input type="password" id="inputPassword" />
+            </label>
+            <label htmlFor="inputURL">
+              URL
+              <input type="text" id="inputURL" />
+            </label>
+            <button type="submit">Cadastrar</button>
+            <button type="reset" onClick={ () => setRegister(false) }>Cancelar</button>
+          </form>
+        ) : <button onClick={ () => setRegister(true) }> Cadastrar nova senha</button>
+    }
+    </div>
   );
 }
 
